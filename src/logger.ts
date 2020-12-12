3b4f2d263b6d9b5e1e60ae928a2699275113de39
Xmader <xmader@outlook.com>
2020-12-12 18:27:55 -0500

    refactor: logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,38 +1,34 @@
 // Logger setup: winston
 
-import winston from "winston";
+import winston, { Logform } from "winston";
 import nconf from "nconf";
 
 export default (): void => {
     if (!winston.format) return;
 
     // From stucco: https://github.com/Toad39/stucco
-    const formats = [];
-    // @ts-ignore
+    const formats: Logform.Format[] = [];
     formats.push(winston.format.colorize());
-    // @ts-ignore
     formats.push((winston.format(msg => {
         // Custom timestamp
         msg.level = `[${Math.floor(Date.now() / 100)}] ${msg.level}`;
         return msg;
     }))());
-    // @ts-ignore
     formats.push(winston.format.splat());
-    // @ts-ignore
     formats.push(winston.format.simple());
-    const fileFormats = [...formats];
-    fileFormats.shift(); // Take off the colorization for files.
+
+    const fileFormats = formats.slice(1); // Take off the colorization for files.
 
     winston.configure({
         level: nconf.get("log:level") || "info",
         transports: [
             new winston.transports.Console({
                 handleExceptions: true,
-                format: winston.format.combine.apply(null, formats)
+                format: winston.format.combine(...formats)
             }),
             new winston.transports.File({
                 filename: nconf.get("log:file") || "../mscz-api.log",
-                format: winston.format.combine.apply(null, fileFormats)
+                format: winston.format.combine(...fileFormats)
             })
         ]
     });
