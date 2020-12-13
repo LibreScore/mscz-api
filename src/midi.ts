b7aa6b7c9c2f1bc0985c689b6d8d85aa90dcb0e6
Toad39 <git@mail.toad39.dev>
2020-12-12 20:07:59 -0800

    feat: Start With MIDI Also refactored imports.

--- src/midi.ts
+++ src/midi.ts
@@ -1,9 +1,9 @@
 import winston from "winston";
 import webmscore from "webmscore";
-import { RequestHandler } from "express";
+import {RequestHandler} from "express";
 
 module.exports = ((req, res) => {
-    winston.http("/meta accessed.");
+    winston.http("/midi accessed.");
     webmscore.ready.then(async () => {
         let score;
         try {  score = await webmscore.load("mscz", req.body, [], false); }
@@ -11,10 +11,11 @@ module.exports = ((req, res) => {
             res.sendStatus(400).send(e);
             return;
         }
+        const soundFontData = new Uint8Array(
+            await (
+                await fetch("ftp://ftp.osuosl.org/pub/musescore/soundfont/MuseScore_General/MuseScore_General.sf3")
+            ).arrayBuffer()
+        );
         const metadata = await score.metadata();
-
-        res.json(metadata);
-
-        score.destroy();
-    });
+    })
 }) as RequestHandler;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 // Web server
 import http from "http";
 import express from "express";
-import {NextFunction} from "express";
-import rawBody from "raw-body";
-import nconf from "nconf";
 import winston from "winston";
 import bodyParser from "body-parser";
 
@@ -11,21 +8,10 @@ const app = express();
 const server = http.createServer(app);
 export {server, app};
 
-// raw body express middleware. https://www.npmjs.com/package/raw-body
-/*
-// @ts-ignore
-app.use((req: Request, res: Response, next: any) => {
-    // @ts-ignore
-    rawBody(req).then(buf => {
-        // @ts-ignore
-        req.body = buf;
-        next();
-    }).catch((e: Error) => {
-        return;
-    });
-});
-*/
-app.use(bodyParser.raw());
+app.use(bodyParser.raw({
+    type: "application/x-musescore",
+    limit: "10mb"
+}));
 
 // Add each route.
 app.post("/meta", require("./meta"));
@@ -34,6 +20,6 @@ app.post("/meta", require("./meta"));
 
 function listen() {
     server.listen(3000, () => {
-        winston.info("hi.");
+        winston.info("mscz-api online.");
     });
 }; export {listen};
