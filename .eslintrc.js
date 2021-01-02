142aebb9ca0cf93408242c120c6bfb11e13c9a68
SealsRock12 <git@mail.toad39.dev>
2021-01-02 13:21:21 -0800

    ci: update deployment script

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,10 @@ module.exports = {
         "semi": [
             "error",
             "always"
+        ],
+        "eol-last": [
+            "error",
+            "alwaysz"
         ]
     }
 };
